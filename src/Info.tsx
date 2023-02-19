d74bae72164bb7c9771b1c3d5070247770188862
Quentin Chang <quentinchang1168@gmail.com>
2023-02-19 13:27:52 +1000

    Test commit, added a dummy image to test functionality

---
+++ src/Info.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import testimg from "./assets/testimg.jpg"
+
+function MovieItem() {
+    
+        return (
+        
+            <>
+                <img src = {testimg} alt="Image"/>
+            </>
+        );
+
+    
+};
+
+export default MovieItem;
